37907b9a3ce081cb1cc41c770a3e3e152585db6e
triet-mesoneer <triet.ngo@mesoneer.com>
2024-03-04 21:15:47 +0700

    chore: update ci file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
+	distDir: 'dist',
+	images: {
+		unoptimized: true
+	}
 };
 
 export default nextConfig;
